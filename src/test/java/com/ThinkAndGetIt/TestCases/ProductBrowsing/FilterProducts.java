000888a540644ae711e3e5b2ae76488c80578350
hirwa <hirwagad767@gmail.com>
2026-05-21 14:43:26 +0200

    ft: added filter bags and luggage test

--- src/test/java/com/ThinkAndGetIt/TestCases/ProductBrowsing/FilterProducts.java
+++ src/test/java/com/ThinkAndGetIt/TestCases/ProductBrowsing/FilterProducts.java
@@ -1,7 +1,31 @@
 package com.ThinkAndGetIt.TestCases.ProductBrowsing;
 
 import com.ThinkAndGetIt.Base.BaseTest;
+import com.ThinkAndGetIt.EndPoints.EndPoints;
+import com.microsoft.playwright.Locator;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.ThinkAndGetIt.EndPoints.EndPoints.HOME;
+import static com.ThinkAndGetIt.EndPoints.EndPoints.Product;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class FilterProducts extends BaseTest {
-
+    @Test
+    public void filterByBagAndLuggage() {
+        String Url = properties.getProperty("baseUrl") + Product;
+        page.navigate(Url);
+        page.waitForTimeout(5000);
+        List<String> bags = page.locator("a div h3").allInnerTexts();
+        System.out.println(bags);
+        for (String itemName : bags) {
+            String normalizedName = itemName.toLowerCase();
+            assertTrue(
+                    normalizedName.contains("bag"),
+                    "not bags only in here! Found an invalid item named: " + itemName
+            );
+        }
+    }
 }
