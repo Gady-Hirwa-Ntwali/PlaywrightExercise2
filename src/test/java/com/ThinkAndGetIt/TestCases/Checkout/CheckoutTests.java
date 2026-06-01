8ddd03c3bd51637e1d500bc455fb64326d40605f
hirwa <hirwagad767@gmail.com>
2026-06-01 14:20:54 +0200

    fx: fix checkout page

--- src/test/java/com/ThinkAndGetIt/TestCases/Checkout/CheckoutTests.java
+++ src/test/java/com/ThinkAndGetIt/TestCases/Checkout/CheckoutTests.java
@@ -1,6 +1,7 @@
 package com.ThinkAndGetIt.TestCases.Checkout;
 
 import com.ThinkAndGetIt.Base.BaseTest;
+import com.ThinkAndGetIt.TestCases.Authentication.CreateAccountTest;
 import com.ThinkAndGetIt.TestCases.Authentication.LoginTest;
 import com.ThinkAndGetIt.TestCases.CArtManagement.AddProductToCart;
 import com.microsoft.playwright.Page;
@@ -23,9 +24,19 @@ public class CheckoutTests extends BaseTest {
 
     @Test
     public void checkoutLoggedIn(){
-        LoginTest.SuccessfulTest();
+        CreateAccountTest.createAccountTestSuccessfully();
         AddProductToCart.addItemByQuickAdd();
         page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Checkout")).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add new address")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First name")).fill("am not ");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Last name")).fill("a human");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Phone number")).fill("234567898765");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Street address")).fill("kigali");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("City")).fill("kigali");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("State / Region")).fill("kigali");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Country")).fill("Rwanda");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Postal code (optional)")).fill("00000");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save Address")).click();
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue to Payment")).click();
         page.getByText("💵Cash on DeliveryPay when").click();
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Review Order")).click();
