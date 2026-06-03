0c23807c443650eae05020e505091ee02dac3944
hirwa <hirwagad767@gmail.com>
2026-06-03 15:37:37 +0200

    fix: fixed pages framework

--- src/test/java/com/ThinkAndGetIt/EndPoints/EndPoints.java
+++
@@ -1,9 +0,0 @@
-package com.ThinkAndGetIt.EndPoints;
-
-public class EndPoints {
-    public static final String HOME = "home";
-    public static final String Product = "products";
-    public static final String Cart = "cart";
-    public static final String Checkout = "checkout";
-    public static final String Login = "login";
-}
