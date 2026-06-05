20b1a154d14ac8352a8880006c99e62c21b79cbc
hirwa <hirwagad767@gmail.com>
2026-06-05 15:03:53 +0200

    ft: added yml file git actions

--- src/test/java/com/ThinkAndGetIt/TestCases/ProductBrowsing/FilterProductByCategory.java
+++ src/test/java/com/ThinkAndGetIt/TestCases/ProductBrowsing/FilterProductByCategory.java
@@ -10,6 +10,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 import java.util.regex.Pattern;
 
 public class FilterProductByCategory extends BaseTest {
+    /.
     private ProductsPage navigateToProductsPage() {
         page.navigate(properties.getProperty("baseUrl") +EndPoints.HOME);
         HomePage homePage = new HomePage(page);
@@ -60,3 +61,4 @@ public class FilterProductByCategory extends BaseTest {
     }
 
 }
+//raise issue on github
